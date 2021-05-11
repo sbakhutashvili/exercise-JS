bdb3fd546b831d665e80611a4b58a7cab5fecf62
sbakhutashvili <sandrobakhutashvili@gmail.com>
2021-05-11 23:47:46 +0400

    last commit project.done

--- Project-js/script.js
+++ Project-js/script.js
@@ -2,7 +2,9 @@ const topics = ["Internet Cats", "Meme's", "Typing", "Space", "Rick and Morty"];
 
 class config {
     constructor() {
-       this.topics = topics;  
+       this.topics = topics; 
+       this.url = 'https://api.giphy.com/v1/gifs/';
+       this.apiKey = 'api_key=aFFKTuSMjd6j0wwjpFCPXZipQbcnw3vB';
     }
 }
 
@@ -16,7 +18,7 @@ class catalog extends config {
         })
     }
     
-    // პრომისი, მოქავს url
+    // პრომისი, მოაქვს url
     fetch(newURL) {
         async function fetchFnc() {
             try {
@@ -43,29 +45,62 @@ class catalog extends config {
             node.parentNode.removeChild(node);
         });
     }
-    // ეს ჯერ დასრულებული არარი :დ
-    replaceTabs() {
+
+    // tab - ის gif url
+    tabURL() {
         let arrBtn = document.querySelectorAll('div.buttons > button');
         arrBtn.forEach((item) => {
             item.addEventListener('click', () =>{
-                const buttonValue = item.textContent.value;
-                let url = `https://api.giphy.com/v1/gifs/search?q=${buttonValue}&api_key=aFFKTuSMjd6j0wwjpFCPXZipQbcnw3vB`;
-                fetch(url);
+                item.value = item.innerHTML;
+                const buttonValue = item.value;
+                let url = `${this.url}search?q=${buttonValue}&limit=12&${this.apiKey}`;
+                this.removeGifs();
+                this.fetch(url);
             })
+        })
+    }
+    // ახალ მასივს ვქმნი აქ
+    addInput(value) {
+        if(value !== '') {
+            this.topics.push(value);
+            this.topics.shift()
+            this.deleteButtons();
+            this.renderButtons();
+            this.tabURL();
+        }
+        
+    }
+    // ძველს ვშლი
+    deleteButtons() {
+        let myNode = document.querySelector('.buttons');
+        myNode.innerHTML = '';
+    }
+    //ტრენდები
+    seeWhatsTrending() {
+        const trendBtn = document.getElementById('seeWhatsTrending');
+        trendBtn.addEventListener('click', () => {
+            let url = `${this.url}trending?&limit=12&${this.apiKey}`;
+            this.fetch(url);
+            this.removeGifs();
         })
     }
 }
 
+// input - ში ჩაწერისას და submit ბათონის დაჭერისას გიფები რო ამოაგდოს
+document.getElementById('submit').addEventListener('click', (event) => {
+    event.preventDefault();
+    const inputValue = document.getElementById('input').value;  
+    let url = `${ctg.url}search?q=${inputValue}&limit=12&${ctg.apiKey}`;
+    ctg.removeGifs();
+    ctg.fetch(url);
+    ctg.addInput(inputValue);
+})
+
+
 const ctg = new catalog();
 
 ctg.renderButtons();
-// inpu - ში ჩაწერისას და submit ბათონის დაჭერისას გიფები რო ამოაგდოს
-document.getElementById('submit').addEventListener('click', (event) => {
-    event.preventDefault();
-    const inputValue = document.getElementById('input').value;
-    let url = `https://api.giphy.com/v1/gifs/search?q=${inputValue}&api_key=aFFKTuSMjd6j0wwjpFCPXZipQbcnw3vB`;
-    ctg.removeGifs();
-    ctg.fetch(url);
-})
 
-ctg.replaceTabs();
+ctg.tabURL();
+
+ctg.seeWhatsTrending();
